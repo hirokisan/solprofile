ac75106aba2e1a824a0e3ebf3b0a28f3e3298885
hirokisan <dev@hirokiueno.com>
2024-10-29 13:38:18 +0900

    chore: fix lint

--- examples/solprofile/src/util.rs
+++ examples/solprofile/src/util.rs
@@ -5,6 +5,6 @@ pub fn get_default_signer_keypair_path() -> String {
 }
 
 pub fn get_profile_account(owner: &Pubkey, program_id: &Pubkey) -> (Pubkey, u8) {
-    pub const PROFILE_SEED: &'static [u8] = b"profile";
+    pub const PROFILE_SEED: &[u8] = b"profile";
     Pubkey::find_program_address(&[PROFILE_SEED, owner.to_bytes().as_ref()], program_id)
 }
